7c2bfc0760ddec35024dab43c58831d0f18206e4
Dima Spitsyn <sqvoch@gmail.com>
2018-02-22 23:41:49 +0300

    end 2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let a = preferences[i];
+    let b = preferences[(a - 1)];
+    let c = preferences[(b - 1)];
+
+    if ((c - 1) === i) {
+      count += 1
+    }
+  }
+  return (Math.floor(count / 3));
 };
